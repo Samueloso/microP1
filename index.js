21d55dcd008655e03cdfe42c9109e9311098e434
samueloso <samuel.cedeno@correo.unimet.edu.ve>
2021-10-17 17:12:57 -0400

    se hizo la skills

---
+++ index.js
@@ -0,0 +1,25 @@
+const color_fondo = document.getElementById("color_fondo");
+const numero = document.getElementById("numero");
+let cantidad = 0;
+let tiempo = setInterval(() => {
+  cantidad += 1;
+  color_fondo.style.height = `${cantidad}`;
+  numero.textContent = cantidad;
+
+  if (cantidad === 20) {
+    clearInterval(tiempo);
+  }
+}, 80);
+
+const color_fondoa = document.getElementById("color_fondo1");
+const numeroa = document.getElementById("numero1");
+let cantidada = 0;
+let tiempo1 = setInterval(() => {
+  cantidada += 1;
+  color_fondo1.style.height = `${cantidada}`;
+  numeroa.textContent = cantidada;
+
+  if (cantidada === 100) {
+    clearInterval(tiempo1);
+  }
+}, 80);
